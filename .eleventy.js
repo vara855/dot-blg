6dec8ae7341748d0b8dc3dc65999cba7374ffbd2
vara855 <valerii.nosikov@icloud.com>
2022-11-19 16:26:06 +0300

    Come back to work under `. blg`

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,73 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const Image = require("@11ty/eleventy-img");
+const path = require("path");
+const { DateTime, Duration } = require("luxon");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+const renderPermalink = require("./renderPermalink.js");
+const slugify = require("slugify");
+
+function imageShortcode(
+  src,
+  alt = "alt",
+  sizes = "(min-width: 1024px) 100vw, 50vw"
+) {
+  console.log(`Generating image(s) from:  ${src}`);
+  const srcPath = path.parse(src);
+  let options = {
+    widths: [600, 900, 1500],
+    formats: ["webp", "jpeg"],
+    urlPath: `/img/${srcPath.dir}`,
+    outputDir: `./_site/img/${srcPath.dir}`,
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src);
+      const name = path.basename(src, extension);
+      return `${name}-${width}w.${format}`;
+    },
+  };
+
+  // generate images
+  Image(`./src/img/${src}`, options);
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+  // get metadata
+  metadata = Image.statsSync(`./src/img/${src}`, options);
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt({
+      breaks: true,
+      html: true,
+      linkify: true,
+    }).use(markdownItAnchor, {
+      permalink: true,
+      slugify: slugify,
+      renderPermalink,
+    })
+  );
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/js");
-  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/icons");
+
+  eleventyConfig.addShortcode("image", imageShortcode);
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight);
 
+  eleventyConfig.addFilter("ttr", (value) => {
+    const words = value.trim().split(/\s+/).length;
+    const time = Math.ceil(words / 225);
+    return Duration.fromObject({ minutes: time }).toHuman();
+  });
   eleventyConfig.addFilter("formatDate", (value) =>
     DateTime.fromJSDate(new Date(value)).toLocaleString(DateTime.DATE_FULL)
   );
---
+++ renderPermalink.js
@@ -0,0 +1,39 @@
+// https://github.com/valeriangalliat/markdown-it-anchor/blob/HEAD/permalink.js
+// This object is required inside the renderPermalink function.
+// It's copied directly from the plugin source code.
+const position = {
+  false: "push",
+  true: "unshift",
+};
+
+// Copied directly from the plugin source code, with one edit
+// (marked with comments)
+const renderPermalink = (slug, opts, state, idx) => {
+  const space = () =>
+    Object.assign(new state.Token("text", "", 0), {
+      content: " ",
+    });
+
+  const linkTokens = [
+    Object.assign(new state.Token("link_open", "a", 1), {
+      attrs: [
+        ["class", opts.permalinkClass],
+        ["href", opts.permalinkHref(slug, state)],
+      ],
+    }),
+    Object.assign(new state.Token("html_block", "", 0), {
+      // Edit starts here:
+      content: `<span aria-hidden="true" class="header-anchor__symbol">#</span>
+      <span class="screen-reader-only">Direct link to this section</span>`,
+      // Edit ends
+    }),
+    new state.Token("link_close", "a", -1),
+  ];
+
+  if (opts.permalinkSpace) {
+    linkTokens[position[!opts.permalinkBefore]](space());
+  }
+  state.tokens[idx + 1].children[position[opts.permalinkBefore]](...linkTokens);
+};
+
+module.exports = renderPermalink;
